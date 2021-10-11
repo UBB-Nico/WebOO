5788dbe741e08b15edd15f073f39b3db41a2c596
Manuel Barrera <manuelbarrera767@gmail.com>
2021-10-11 14:46:24 -0300

    first commit

---
+++ src/test/java/cl/ubb/tswoo/bacatapp/BacatappApplicationTests.java
@@ -0,0 +1,13 @@
+package cl.ubb.tswoo.bacatapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BacatappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
